bb8f1e072eba096ab60a6046ec95d8daaee4f4a7
Jakob Bæk Tejs Knudsen <jakobtejs@gmail.com>
2016-04-02 20:31:05 +0200

    Made it possible to see how many children you've bought

--- RoedeKors/app/src/main/java/com/example/jakob/roedekors/MinSideActivity.java
+++ RoedeKors/app/src/main/java/com/example/jakob/roedekors/MinSideActivity.java
@@ -2,6 +2,7 @@ package com.example.jakob.roedekors;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.TextView;
 
 public class MinSideActivity extends MainActivity {
     int MYID = R.id.action_min_side;
@@ -10,6 +11,20 @@ public class MinSideActivity extends MainActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_min_side);
+
+        int donation = 200;
+
+        TextView totalDonation = (TextView) findViewById(R.id.totaldonation);
+        TextView donationFamilie = (TextView) findViewById(R.id.donationFamilie);
+        TextView donationBoern = (TextView) findViewById(R.id.donationBoern);
+        TextView donationMyg = (TextView) findViewById(R.id.donationMyg);
+        TextView donationBarsel = (TextView) findViewById(R.id.donationBarsel);
+
+        totalDonation.setText(Integer.toString(donation));
+        donationFamilie.setText(Integer.toString(donation / 430));
+        donationBoern.setText(Integer.toString(donation / 35));
+        donationMyg.setText(Integer.toString(donation / 50));
+        donationBarsel.setText(Integer.toString(donation / 100));
     }
 
 }
