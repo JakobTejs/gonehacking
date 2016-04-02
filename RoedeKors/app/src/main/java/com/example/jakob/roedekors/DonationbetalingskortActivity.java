e944587ae1f33c7076bbe017617dcb8fd7af8872
peterlunding <plj@asseco.dk>
2016-04-02 17:25:24 +0200

    Made coooooool slider

---
+++ RoedeKors/app/src/main/java/com/example/jakob/roedekors/DonationbetalingskortActivity.java
@@ -0,0 +1,39 @@
+package com.example.jakob.roedekors;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class DonationbetalingskortActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donationbetalingskort);
+
+        SeekBar seekBar = (SeekBar)findViewById(R.id.seekBarDonation);
+        final TextView seekBarValue = (TextView)findViewById(R.id.beloeb);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,
+                                          boolean fromUser) {
+                // TODO Auto-generated method stub
+                seekBarValue.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+        });
+    }
+
+}
--- roedekors/app/src/main/java/com/example/jakob/roedekors/DonerActivity.java
+++ roedekors/app/src/main/java/com/example/jakob/roedekors/DonerActivity.java
@@ -1,7 +1,10 @@
 package com.example.jakob.roedekors;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class DonerActivity extends MainActivity {
     int MYID = R.id.action_doner;
@@ -10,6 +13,16 @@ public class DonerActivity extends MainActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doner);
+
+        Button donerKnap = (Button) findViewById(R.id.betalingskort);
+        final DonerActivity tmp = this;
+        donerKnap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(tmp, DonationbetalingskortActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
 
 }
